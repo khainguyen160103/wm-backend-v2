60e27ea7961f834298e6042616859f15d708f89e
quangtranvan8821 <quangtrn8821@gmail.com>
2023-02-18 13:55:05 +0700

    update docker, connect to mr_user db

--- apps/main/src/app.module.ts
+++ apps/main/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
+const schemaName = 'mr_user'
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: process.env.MYSQL_DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'], // get all entities
+      host: process.env.MYSQL_HOST,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      name: schemaName,
+      logging: false,
+      synchronize: false,
+      namingStrategy: new SnakeNamingStrategy()
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
