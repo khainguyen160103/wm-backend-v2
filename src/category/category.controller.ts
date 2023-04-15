73b3d8920cdb03c7ee66a3e6c1b5f1b4564444e6
Quang Trần Văn <quangtrn8821@gmail.com>
2023-04-15 13:12:17 +0700

    update category route

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
 import { CategoryService } from './category.service'
+import { Category } from './entities'
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  getAll(query: any): Promise<Category[]> {
+    return this.categoryService.getByCondition(query)
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  getById(@Param('id') id: string | number): Promise<Category> {
+    return this.categoryService.getById(id)
+  }
 }
