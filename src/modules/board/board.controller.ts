2e05d0ce0c1afd9f76894ed81d99a7e0b072386e
Quang Trần Văn <quangtrn8821@gmail.com>
2023-11-06 23:40:33 +0700

    format

--- src/modules/board/board.controller.ts
+++ src/modules/board/board.controller.ts
@@ -4,35 +4,35 @@ import { CreateBoardDto, UpdateBoardDto } from './dto'
 
 @Controller('board')
 export class BoardController {
-  constructor(private boardService: BoardService) { }
-  
+  constructor(private boardService: BoardService) {}
+
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  async create(@Body() dto: CreateBoardDto) { 
-    return await this.boardService.create(dto as any);
+  async create(@Body() dto: CreateBoardDto) {
+    return await this.boardService.create(dto as any)
   }
 
-  @Get('/:id_board')
+  @Get('/:board_id')
   @HttpCode(HttpStatus.FOUND)
-  async get(@Param('id_board') id: number) { 
-    return await this.boardService.getByCondition({id})
+  async get(@Param('board_id') id: number) {
+    return await this.boardService.getByCondition({ id })
   }
 
   @Get()
   @HttpCode(HttpStatus.FOUND)
-  async getAll() { 
+  async getAll() {
     return this.boardService.getByCondition()
   }
-    
+
   @Put()
   @HttpCode(HttpStatus.OK)
   async update(@Body() dto: UpdateBoardDto) {
-    return await this.boardService.update(dto.id, dto as any);
+    return await this.boardService.update(dto.id, dto as any)
   }
 
-  @Delete('/:id_board')
+  @Delete('/:board_id')
   @HttpCode(HttpStatus.OK)
-  async delete(@Param('id_board') id: number) { 
-    return await this.boardService.delete(id);
+  async delete(@Param('board_id') id: number) {
+    return await this.boardService.delete(id)
   }
 }
