b654dd4744abb9c6667a56205ce721288a8da8df
Quang Trần Văn <quangtrn8821@gmail.com>
2023-11-12 11:56:24 +0700

    update

--- src/modules/tag/tag.controller.ts
+++ src/modules/tag/tag.controller.ts
@@ -1,40 +1,39 @@
-import { Controller, HttpCode, HttpStatus, Post , Body , Get , Param , Put, Delete} from '@nestjs/common'
+import { Controller, HttpCode, HttpStatus, Post, Body, Get, Param, Put, Delete } from '@nestjs/common'
 import { TagService } from './tag.service'
 import { CreateTagDto } from './dto'
 import { UpdateTagDto } from './dto/update-tag.dto'
 
 @Controller('tag')
 export class TagController {
-  constructor(private tagService: TagService) { }
-  
+  constructor(private tagService: TagService) {}
+
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  async create(@Body() dto : CreateTagDto) { 
+  async create(@Body() dto: CreateTagDto) {
     return await this.tagService.create(dto as any)
   }
 
-  @Get('/:id_tag')
+  @Get('/:tag_id')
   @HttpCode(HttpStatus.FOUND)
-  async get(@Param('id_tag') id: string) { 
-    return await this.tagService.getByCondition({id})
+  async get(@Param('tag_id') id: string) {
+    return await this.tagService.getByCondition({ id })
   }
 
   @Get()
   @HttpCode(HttpStatus.FOUND)
-  async getAll() { 
+  async getAll() {
     return await this.tagService.getByCondition()
   }
 
   @Put()
   @HttpCode(HttpStatus.OK)
-  async update(@Body() dto: UpdateTagDto) { 
-    return await this.tagService.update(dto.id, dto as any);
+  async update(@Body() dto: UpdateTagDto) {
+    return await this.tagService.update(dto.id, dto as any)
   }
 
   @Delete('/:id_tag')
   @HttpCode(HttpStatus.OK)
   async delete(@Param('id_tag') id: string) {
-    return await this.tagService.delete(id);
+    return await this.tagService.delete(id)
   }
-
 }
